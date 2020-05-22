01009e3fbd7122171817c2979ed80e5b42e8ddb4
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-05-22 12:34:34 +0530

    Context reducer todo

---
+++ 6-context-reducer-todo/src/Components/Todos.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react'
+import {ListGroup, ListGroupItem} from "reactstrap"
+import {FaRegWindowClose} from "react-icons/fa"
+import {TodoContext} from "../context/TodoContext"
+import {REMOVE_TODO} from "../context/action.types"
+
+
+const Todos = () => {
+    const {todos, dispatch} = useContext(TodoContext);
+
+    return (
+        <ListGroup className="mt-5 mb-2 items">
+            {todos.map(todo => (
+                <ListGroupItem key={todo.id}>
+                    {todo.todoString}
+                <span className="float-right" style={{fontSize:"15px"}} 
+                onClick={() => {
+                    dispatch({
+                        type: REMOVE_TODO,
+                        payload: todo.id
+                    })
+                }}
+                >
+                    <FaRegWindowClose/>
+                </span>
+                </ListGroupItem>
+            ))}
+        </ListGroup>
+    )
+}
+export default Todos;
