ba6eee914346ade6a0dd3863094149af74a22071
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-05-24 11:50:48 +0530

    Firebase-Github-App

---
+++ 10-firebase-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default  {
+    apiKey: "AIzaSyAonfTUfq_dK7BA6IPAdzx9Hxa48A3rwiM",
+    authDomain: "my-gitapp.firebaseapp.com",
+    databaseURL: "https://my-gitapp.firebaseio.com",
+    projectId: "my-gitapp",
+    storageBucket: "my-gitapp.appspot.com",
+    messagingSenderId: "150701916228",
+    appId: "1:150701916228:web:97e9861e5665cfa26bfc11",
+    measurementId: "G-R8E2FQGM4E"
+  };
